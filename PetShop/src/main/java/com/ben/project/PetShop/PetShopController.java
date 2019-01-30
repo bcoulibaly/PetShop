bff796dbbc4f4d691f5bffc00f015081a665978a
Ben Inza Coulibaly <coulibaly@encoway.de>
2019-01-30 17:29:58 +0100

    vd

--- PetShop/src/main/java/com/ben/project/PetShop/PetShopController.java
+++ PetShop/src/main/java/com/ben/project/PetShop/PetShopController.java
@@ -12,7 +12,7 @@ public class PetShopController {
 
     private List<Animals> listAnimals = Arrays.asList(new Animals(1, "Bijou", 25), new Animals(2, "Milou", 60), new Animals(3, "Ragou", 70));
 
-    @GetMapping("/Animals/")
+    @GetMapping("/Animals/{id}")
     public String getAnimalsById(@PathVariable int id) {
         for (int j = 0; j < listAnimals.size(); j++) {
             if (id == listAnimals.get(j).getId()) {
