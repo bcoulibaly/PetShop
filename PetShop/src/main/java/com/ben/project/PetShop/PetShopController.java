02ec0b302cefb2926b59a62bd671d6870478acc1
Ben Inza Coulibaly <coulibaly@encoway.de>
2019-01-31 18:17:07 +0100

    w

--- PetShop/src/main/java/com/ben/project/PetShop/PetShopController.java
+++ PetShop/src/main/java/com/ben/project/PetShop/PetShopController.java
@@ -1,17 +1,19 @@
 package com.ben.project.PetShop;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetShopController {
 
-    private List<Animals> listAnimals = Arrays.asList(new Animals(1, "Bijou", 25), new Animals(2, "Milou", 60), new Animals(3, "Ragou", 70));
+    private List<Animals> listAnimals = new ArrayList<>(Arrays.asList(new Animals(1, "Bijou", 25), new Animals(2, "Milou", 60), new Animals(3, "Ragou", 70)));
 
     @GetMapping("/Animals/{id}")
     public Animals getAnimalsById(@PathVariable int id) {
@@ -38,5 +40,10 @@ public class PetShopController {
             }
         }
         return null;
+    }
+
+    @PostMapping("/Animals")
+    public void addAnimal(@RequestBody Animals identifier) {
+        listAnimals.add(identifier);
     }
 }
